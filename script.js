afd8b9f8a53c37ad83cae54258a61c229bb4ecaa
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-12-04 23:07:05 +0000

    made the first card

--- script.js
+++ script.js
@@ -5,8 +5,31 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // const rootElem = document.getElementById("root");
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  let ulList = document.getElementById("ul-list");
+
+  let liList = document.createElement('li');
+  ulList.appendChild(liList);
+  liList.classList.add("li-list-cls")
+
+  let episodeTitle = document.createElement("h2");
+  episodeTitle.innerHTML = episodeList[0].name;
+  episodeTitle.classList.add("h2-title-cls");
+
+  let episodePicture = document.createElement("img");
+  episodePicture.src = episodeList[0].image.medium;
+  episodePicture.classList.add("img-picture-cls");
+
+  let episodeSummary = document.createElement("p");
+  episodeSummary.innerHTML = episodeList[0].summary;
+  episodeSummary.classList.add("p-summary-cls")
+
+  liList.appendChild(episodeTitle);
+  liList.appendChild(episodePicture);
+  liList.appendChild(episodeSummary);
+  
+
 }
 
 window.onload = setup;
