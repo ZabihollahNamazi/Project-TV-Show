02969d72423b9972c1a81655bcb1371be4642206
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-12-16 15:03:02 +0000

    level 200 search

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 //You can edit ALL of the code here
+const allEpisodes = getAllEpisodes();
+  const state = {
+    allEpisodes,
+    searchTerm:""
+  }
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  
+  makePageForEpisodes(state.allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
@@ -9,22 +15,27 @@ function makePageForEpisodes(episodeList) {
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   let ulList = document.getElementById("ul-list");
 
+  let filteredEpisodes = state.allEpisodes.filter(function(episode){
+    return(episode.name.toLowerCase().includes(state.searchTerm.toLowerCase()))
+  })
+
   //for(let item in episodeList){
-  let cards = episodeList.map ((item)=> {
+  let cards = filteredEpisodes.map ((item)=> {
     return createFilmCard(item)
   })
   ulList.append(...cards);
+
+  let searchEpisodeLabel = document.querySelector("#search-episode-label");
+  searchEpisodeLabel.innerHTML = `Displaying ${filteredEpisodes.length}/${state.allEpisodes.length} episodes`;
  
   // let sss = episodeList.filter(x=> {
   //   return x.name === "Lord Snow"
   // })
   // console.log(sss);
   // //}
-
-  
-  
-
 }
+
+
 
 function createFilmCard(item) {
   // You should have an implementation of this function from before.
@@ -55,5 +66,24 @@ function createFilmCard(item) {
     liList.appendChild(source);
     return liList; 
 }
+
+
+let searchEpisode = document.querySelector("#search-episode");
+searchEpisode.addEventListener('keyup', ()=> {
+  state.searchTerm = searchEpisode.value;
+  let bodyUl = document.querySelector("#ul-list");
+  bodyUl.innerHTML = "";
+
+  
+  //console.log(state.searchTerm)
+
+//   let filteredEpisodes = state.allEpisodes.filter(function(episode){
+//     return(episode.name.toLowerCase().includes(state.searchTerm.toLowerCase()))
+//   })
+// console.log(filteredEpisodes)
+makePageForEpisodes()
+})
+
+
 
 window.onload = setup;
